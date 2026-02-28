7ff7cb77e0eb2e764eb506ebbd0e049bb7d551af
Amr Nabih <amrnabih112@gmail.com>
2026-02-28 18:36:07 +0200

    feat: Using StrongTyped Configurations

--- DTOs/Auth/RegisterResponseDto.cs
+++ DTOs/Auth/RegisterResponseDto.cs
@@ -1,7 +1,7 @@
 namespace ECommerce.DTOs.Auth
 {
     public class RegisterResponseDto
-    {
+    {   
         public required string UserId { get; set; }
         public required string Email { get; set; }
     }
